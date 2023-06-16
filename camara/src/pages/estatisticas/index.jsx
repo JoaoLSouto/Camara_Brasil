d4fb8f4d56715b6b1860655acd8516159bb224c0
pedroqwertyu <pedro.h.m.f30@gmail.com>
2023-06-15 21:53:31 -0300

    intregado os dados

--- camara/src/pages/estatisticas/index.jsx
+++ camara/src/pages/estatisticas/index.jsx
@@ -4,10 +4,7 @@ import { Subheader } from '../../components/Subheader';
 import './index.css';
 import { Bottom } from '../../components/Bottom';
 import axios from "axios";
-import { BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
-import { Tooltip } from "react-bootstrap";
-import { Legend } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from "recharts";
 
 const Estatisticas = () => {
 
@@ -25,9 +22,10 @@ const Estatisticas = () => {
           const qtd = item.siglaPartido in obj ? obj[item.siglaPartido] + 1 : 1
           obj[item.siglaPartido] = qtd
         })
-        console.log(obj);
 
-        setPartidos(obj)
+        const arrDeObj = Object.entries(obj).map(([chave, valor]) => ({ 'partido': chave, 'valor': valor }));
+
+        setPartidos(arrDeObj)
       } catch (error) {
         console.error('Erro ao buscar deputados:', error);
       }
@@ -36,26 +34,15 @@ const Estatisticas = () => {
     fetchData();
   }, []);
 
-  const data = [
-    {name: 'Page A', pv: 2400, amt: 2400},
-    {name: 'Page B', pv: 1398, amt: 2210},
-    {name: 'Page C', pv: 9800, amt: 2290},
-    {name: 'Page D', pv: 3908, amt: 2000},
-    {name: 'Page E', pv: 4800, amt: 2181},
-    {name: 'Page F', pv: 3800, amt: 2500},
-    {name: 'Page G', pv: 4300, amt: 2100},
-  ];
-
-
   return (
     <div>
       <Subheader />
       <Header />
-      <BarChart width={730} height={250} data={data}>
+      <BarChart width={1250} height={500} data={partidos}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis  dataKey="pv"/>
-        <Bar dataKey="pv" fill="#aaaaaa" />
+        <XAxis dataKey="partido" />
+        <YAxis />
+        <Bar dataKey="valor" fill="#8884d8" />
         <Tooltip />
         <Legend />
       </BarChart>
